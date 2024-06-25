394f82fb7bce2379bac69fe9d2e6d0d5824397b2
Hammad-Rehman <124163466+developer-hammad-rehman@users.noreply.github.com>
2024-06-25 17:45:31 +0500

    solving aptrosphy errors

--- src/app/components/Remark.tsx
+++ src/app/components/Remark.tsx
@@ -8,7 +8,7 @@ export default function Remark() {
   return (
     <div className='px-4 py-4 w-full h-full flex flex-col gap-5 justify-center items-center'>
         <h3 className='text-3xl font-bold text-center py-4'>FREE REMAKE GUARANTEE</h3>
-        <p className='text-center font-bold text-xs'>Our comprehensive, no-fuss, free remake guarantee takes the worry out of ordering. If your garment doesn't fit as you want it to, we'll replace it with a new one free of charge.</p>
+        <p className='text-center font-bold text-xs'>Our comprehensive, no-fuss, free remake guarantee takes the worry out of ordering. If your garment doesn&apos;t fit as you want it to, we&apos;ll replace it with a new one free of charge.</p>
         <button className='border border-black py-2 px-5 italic rounded-lg'>Get More Information</button>
         <div className='flex flex-wrap lg:flex-nowrap justify-center items-center gap-3'>
             <div className='flex flex-col justify-center items-center gap-4'>
